3f174aa85da273129980eac48c1d9d36f53e3fc2
Raphael Ovonlen <reoundaunted@gmail.com>
2022-02-15 13:19:14 +0100

    Assignment Folder

---
+++ Calculator/app.js
@@ -0,0 +1,34 @@
+let display = document.getElementById('display');
+
+let buttons = Array.from(document.getElementsByClassName('button'));
+// console.log(buttons);
+
+// adding a click event listener
+buttons.map(button => {
+    button.addEventListener('click', (e) => {
+        // console.log('clicked');
+        // console.log(e);
+        // console.log(e.target);
+        // console.log(e.target.innerText);
+        switch (e.target.innerText) {
+            case 'C':
+                display.innerText = '';
+                break;
+            case '←':
+                if (display.innerText) {
+                    display.innerText = display.innerText.slice(0, -1);
+                }
+                break;
+            case '=':
+                try {
+                    display.innerText = eval(display.innerText);
+                } catch {
+                    display.innerText = 'Err!';
+                }
+
+                break;
+            default:
+                display.innerText += e.target.innerText;
+        }
+    });
+});
